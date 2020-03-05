7bf5232996db114feafdc44340138db8d3a27bea
Lorena Gomes <42523044+Lorenagoc@users.noreply.github.com>
2020-03-04 22:28:16 -0300

    Update EXE1 - Programação Orientada a Objetos.py

--- EXE1 - Programação Orientada a Objetos.py
+++ EXE1 - Programação Orientada a Objetos.py
@@ -1,8 +1,11 @@
 class Autor():
+
     def __init__(self, primeiro_nome, nome_meio, ultimo_nome, data_nascimento):
 
         self.primeiro_nome = primeiro_nome
-        # o atributo >> nome_meio << é opcional
+
+        # O atributo >> nome_meio << é opcional
+
         if nome_meio == "":
             self.nome_meio = ''
         else:
@@ -10,17 +13,28 @@ class Autor():
         self.ultimo_nome = ultimo_nome
         self.data_nascimento = data_nascimento
 
+    @property
     def nome_como_citado(self):
-        # o método >> upper() << é o responsável por deixar todas as letras da string maiúsculas
+
+        # O método >> upper() << é o responsável por deixar todas as letras da string maiúsculas
+        # A posição 0 no atributo self.primeiro_nome[0] pega a primeira letra do primeiro nome
+
         return "{ultimo_nome} {primeiro_nome}.".format(ultimo_nome=self.ultimo_nome.upper(),
                                                        primeiro_nome=self.primeiro_nome[0].upper())
 
-    def __str__(self):
-        return self.nome_como_citado()
+    def atributos_autor(self):
 
+        return "\n******** Atributos do autor ********\nAutor: {first} {second} {last}\nData de nascimento: {date}\n".format(first=self.primeiro_nome, second=self.nome_meio, last=self.ultimo_nome, date=self.data_nascimento)
+
+    def __str__(self):
+        return self.nome_como_citado
+
+#*****************************************************************************************************************
 
 class Livro():
     def __init__(self, titulo, ano):
+
+        # O título não pode ser vazio, caso contrário, uma mensagem de erro é encaminhada ao usuário
 
         if titulo == "":
             return ValueError("Erro! O título do livro não pode ser vazio!")
@@ -45,10 +59,14 @@ class Livro():
     def adiciona_autores(self, autor):
         self.lista_autores.append(autor)
 
+    # O método join pega todos os ítens em um iterador e os une em uma string.
+
     def __str__(self):
         str1 = ' '.join([str(elemento) for elemento in self.lista_autores])
-        return "\nTítulo: {titulo}\nAno: {ano}\nAutores: {autores}\n".format(titulo=self.titulo, ano=self.ano,
+        return "\n******** Atributos do livro ********\nTítulo: {titulo}\nAno: {ano}\nAutores: {autores}\n".format(titulo=self.titulo, ano=self.ano,
                                                                              autores=str1)
+
+#*****************************************************************************************************************
 
 class Biblioteca():
     def __init__(self):
@@ -58,18 +76,63 @@ class Biblioteca():
     def adiciona_livros(self, livro):
         self.lista_livros.append(livro)
 
-    def livros_por_autor(self, autor):
+    def livros_por_autor(self, obj_autor):
 
-        #As chaves do acervo foram definidas na main. Aqui acessamos uma por uma e armazenamos em uma lista auxiliar
-        aux_autores = []
-        for item_autores in self.acervo:
-            aux_autores.append(item_autores)
-    
-        aux_livros = []
-        for item_livros in self.lista_livros:
-            aux_livros.append(item_livros)
-            
-        #Falta terminar essa classe        
+        # A chave do acervo (dicionário) é definida como sendo o autor em questão
+        self.acervo.update({obj_autor.nome_como_citado: []})
+
+        # O conteúdo da lista de livros é convertido de objeto para string
+        livro_converter_obj_str = ' '.join([str(elemento) for elemento in self.lista_livros])
+
+        # Separamos o conteúdo da string anteriormente formada um em cada linha
+        lista_livros_organizada = livro_converter_obj_str.split('\n')
+
+        # Percorre o acervo de livros
+        for autor in self.acervo:
+
+            # Lista que armazenará os livros que o autor em questão participou/escreveu
+            livros_escritos_autor = []
+
+            # Esse será um iterador que usaremos para pegar na lista, através do índice da posição, o título do livro
+            pegando_titulo_livro = 1
+
+            # Percorre a lista de livros anteriormente organizada (com um parâmetro do livro em cada linha)
+            for livro in lista_livros_organizada:
+
+                # Se o autor tiver como autor do livro...
+                if autor in livro:
+
+                    #Pegamos dentro da lista o título do livro (na primeira rodada do for o titulo fica na posição 1)
+                    titulo_livro = lista_livros_organizada[pegando_titulo_livro]
+
+                    # Para fins estéticos do programa, deixamos apenas o nome do livro, sem 'Título:' antes
+                    titulo_livro = titulo_livro[8:]
+
+                    # Adicionamos, na lista de livros escritos pelo autor, o titulo do livro que acabamos de encontrar
+                    livros_escritos_autor.append(titulo_livro)
+
+                    # Passamos para a próxima posição onde terá o título do próximo livro
+                    pegando_titulo_livro = pegando_titulo_livro + 4
+
+            # Fazemos um update no acervo, adicionando na chave um novo valor
+            self.acervo.update({autor: livros_escritos_autor})
+
+        # Imprime os livros do autor pesquisado percorrendo o acervo
+        print('\n*************** Livros do autor pesquisado ***************')
+
+        for nome_autor, livros_autor in self.acervo.items():
+            print(nome_autor, ':', livros_autor)
+
+        return ''
+
+    def __str__(self):
+
+        str1 = ' '.join([str(elemento) for elemento in self.lista_livros])
+        return "********* LIVROS CONTIDOS NO ACERVO *********\n{livros}".format(livros=str1)
+
+#*****************************************************************************************************************
+
+# Instanciação dos objetos
 
 livro1 = Livro('Física Experimental', '2008')
 livro2 = Livro('Livro Fictício Elmo', '2010')
@@ -78,19 +141,21 @@ agostinho = Autor('Agostinho', 'Aurélio', 'Campos', '12/12/1964')
 elmo = Autor('Elmo', 'Salomão', 'Alves', '12/12/1960')
 nivaldo = Autor('Nivaldo', 'Lúcio', 'Speziali', '12/12/1963')
 
+# Imprime os atributos do autor
+print(agostinho.atributos_autor())
+print(elmo.atributos_autor())
+print(nivaldo.atributos_autor())
+
 livro1.adiciona_autores(agostinho)
 livro1.adiciona_autores(elmo)
 livro1.adiciona_autores(nivaldo)
 livro2.adiciona_autores(elmo)
 
-#print(livro1)
-#print(livro2)
-
 biblioteca = Biblioteca()
 biblioteca.adiciona_livros(livro1)
 biblioteca.adiciona_livros(livro2)
 
-#Definimos as chaves e os valores de cada uma
-biblioteca.acervo.update({agostinho.nome_como_citado: [], elmo.nome_como_citado: [], nivaldo.nome_como_citado: []})
-
+# Imprime tudo que tem na biblioteca
+print(biblioteca)
+# Pesquisamos um autor específico dentre os disponíveis
 print(biblioteca.livros_por_autor(elmo))
